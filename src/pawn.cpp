cbdb40730e279adead7e632728c58adbcec04a06
AliKhattak-91 <alikhattak8668@gmail.com>
2026-02-08 15:06:53 +0500

    Implemented proper pawn movement

--- src/pawn.cpp
+++ src/pawn.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "board.hpp"
 #include "pawn.hpp"
 
 using namespace std;
@@ -14,11 +15,35 @@ Pawn::Pawn() {
   pawnSprite.setScale(0.9, 0.9);
 }
 
-void Pawn::pawnMovement() {
-  if (isSelected) {
+void Pawn::pawnMovement(Board chessboard[][8], Pawn *piece, int indexX,
+                        int indexY) {
+  if (Pawn::isSelected) {
     // move the pawn only if selected
     // move the pawn only if the squares are available
     // move the pawn only if it can capture anything
     //      -> make another function to check if capture is possible or not
+
+    // Pawn Movement Rules
+    // 1) can only move upwards = check
+    // 2) can only move one square = check
+    // 3) can only move two squares if it is the first move
+
+    if (indexX == piece->currentSquare->squareIndices.x - 1 &&
+        indexY == piece->currentSquare->squareIndices.y &&
+        piece->currentSquare->squareIndices.x != -1) {
+
+      if (chessboard[indexX][piece->currentSquare->squareIndices.y].isEmpty) {
+        chessboard[indexX][piece->currentSquare->squareIndices.y]
+            .removePreviousPiece(Pawn::currentSquare);
+        chessboard[indexX][piece->currentSquare->squareIndices.y]
+            .setCurrentPiece(piece);
+        Pawn::currentSquare =
+            &chessboard[indexX][piece->currentSquare->squareIndices.y];
+        Pawn::isSelected = false;
+      }
+    }
+
+    cout << piece->currentSquare->squareIndices.x << " "
+         << piece->currentSquare->squareIndices.y << endl;
   }
 }
